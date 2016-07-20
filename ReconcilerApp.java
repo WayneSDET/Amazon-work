b09cd6232e485782b6ac758f7e0fe74c22c66c82
Wayne Bruce <wayne.sdet@gmail.com>
2016-07-20 12:14:17 -0400

    Update ReconcilerApp.java

--- ReconcilerApp.java
+++ ReconcilerApp.java
@@ -3,7 +3,6 @@ package amazon.dtp.script.reconciler;
 public class ReconcilerApp {
 
     public static void main(String[] args) {
-        @SuppressWarnings("unused")
         ReconcilerAppFrame ReconcileApp = new ReconcilerAppFrame();
     }
 }
